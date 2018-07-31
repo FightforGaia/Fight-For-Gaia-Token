33f36dd1a435ae2923d9199a4106005915d6c681
Sir. Quetzalcóatl <Dsssidi@gmail.com>
2018-07-30 22:13:58 -0400

    2. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var FgaiaToken = artifacts.require("./FgaiaToken.sol");
+var FgaiaTokenSale = artifacts.require("./FgaiaTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FgaiaToken);
+  deployer.deploy(FgaiaToken, 1000000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(FgaiaTokenSale, FgaiaToken.address, tokenPrice);
+  });
 };
